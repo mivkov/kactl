da8b2fd0aae7c249de662fb6d9704a1c3b83da87
Misha Ivkov <mishai@stanford.edu>
2023-06-01 09:51:52 -0700

    add hashes

--- content/data-structures/SegmentTree.h
+++ content/data-structures/SegmentTree.h
@@ -17,7 +17,7 @@ template <class T, T (*f)(T, T), T (*unit)()> struct Seg {
 			s[pos] = f(s[pos * 2], s[pos * 2 + 1]);
 	}
 	T query(int b, int e) { // query [b, e)
-		T ra = unit, rb = unit;
+		T ra = unit(), rb = unit();
 		for (b += n, e += n; b < e; b /= 2, e /= 2) {
 			if (b % 2) ra = f(ra, s[b++]);
 			if (e % 2) rb = f(s[--e], rb);
